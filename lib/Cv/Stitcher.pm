cd1bb0085cdb59725418bfc9f53cb73573832406
KUBO, Koichi <k@obuk.org>
2013-03-02 20:21:46 +0900

    reflects changes in Cv::Config

--- lib/Cv/Stitcher.pm
+++ lib/Cv/Stitcher.pm
@@ -16,7 +16,7 @@ our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = ();
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 require XSLoader;
 XSLoader::load('Cv::Stitcher', $VERSION);
